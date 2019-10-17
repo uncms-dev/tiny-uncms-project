aea413493fd3446e7731a8311837f0bf35e02568
Lewis Collard <lewiscollard@gmail.com>
2019-10-17 02:26:57 +0100

    Document `content.models`.

--- tiny_project/apps/__init__.py
+++ tiny_project/apps/__init__.py
@@ -1,1 +1,7 @@
 """Custom applications installed by tiny-cms-project project."""
+
+#
+# And a quick note: nothing in the CMS requires that you have your apps in
+# a folder called `apps` in your project. It doesn't care where they live at
+# all. We use this convention at Onespacemedia.
+#
--- tiny_project/apps/content/models.py
+++ tiny_project/apps/content/models.py
@@ -7,14 +7,29 @@ class Content(ContentBase):
     # We can define no fields at all here! Just its existence as a
     # non-abstract class inheriting from ContentBase will make it available
     # in the page types.
+    #
+    # We don't have to specify a urlconf as we did in the news app. If you
+    # don't specify one, it will default to rendering the template at
+    # `<app_label>/<modelname>.html` - i.e. `content/content.html` in this case.
     pass
 
 
 class ContentSection(models.Model):
+    # This is a model which will be registered inline so that you can edit it
+    # directly from the page's admin screen.
+    #
+    # This ForeignKey to `pages.Page` is entirely necessary in order to make
+    # inlines work. Note that it is to the *Page* itself and not the content
+    # model!
+    page = models.ForeignKey(
+        'pages.Page',
+    )
+
     title = models.CharField(
         max_length=100,
     )
 
+    # HtmlField is explained in more depth over in the news app.
     text = HtmlField(
         null=True,
         blank=True,
