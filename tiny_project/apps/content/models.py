9907dda2d0408488244555f22176b6be36acce0d
Lewis Collard <lewiscollard@gmail.com>
2019-10-26 14:37:26 +0100

    Implement CMS 4.1.0 get_searchable_text.

--- tiny_project/apps/content/models.py
+++ tiny_project/apps/content/models.py
@@ -11,7 +11,23 @@ class Content(ContentBase):
     # We don't have to specify a urlconf as we did in the news app. If you
     # don't specify one, it will default to rendering the template at
     # `<app_label>/<modelname>.html` - i.e. `content/content.html` in this case.
-    pass
+
+    def get_searchable_text(self):
+        # If you use Watson for your search on your frontend (you really
+        # should, it rules), then by default the Page model will be registered
+        # with it. The search adapter for the Page model will smoosh together
+        # all of the text fields (CharField and TextField) into the blob of
+        # text that Watson searches with a low weighting.
+        #
+        # That works fine, but in our case all of our page's textual content
+        # is in ContentSection objects. To make Watson see this text, we
+        # just need to override get_searchable_text on our content model and
+        # throw in all of the text of our ContentSection too.
+        text = super().get_searchable_text()
+        return ' '.join([text] + [
+            section.title + ' ' + (section.text or '')
+            for section in self.page.contentsection_set.all()
+        ])
 
 
 class ContentSection(models.Model):
