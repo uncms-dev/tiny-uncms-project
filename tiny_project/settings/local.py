2aaeabdf8943b8c7da300eead0c833c1e8cc8b48
Lewis Collard <lewis.collard@onespacemedia.com>
2019-12-05 11:30:41 +0000

    Get SECRET_KEY from environment in CI.

--- tiny_project/settings/local.py
+++ tiny_project/settings/local.py
@@ -12,7 +12,7 @@ TEMPLATES[0]['OPTIONS']['auto_reload'] = DEBUG
 MEDIA_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'media'))
 STATIC_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'static'))
 
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 SITE_DOMAIN = 'localhost:8000'
 
